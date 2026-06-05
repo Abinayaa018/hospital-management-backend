eedecbcd44af64e961d731944f7091146a12270e
Abinayaa018 <abinayaa.a2024cse@sece.ac.in>
2026-06-05 15:00:40 +0530

    Update CORS with correct Vercel frontend URL

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: [
-    "https://kenko-hospital-system.vercel.app",
-    /\.vercel\.app$/,
+    "https://hospital-management-system-drab-beta.vercel.app",
     "http://localhost:3000"
   ],
   credentials: true
